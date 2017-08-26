74a1d07fe2e0a2c8dc830924d10835f3214e3e24
Gerson Moraes <gerson@digirati.com.br>
2017-08-26 18:26:30 -0300

    Updating available bootstrap plugins

--- src/ppx_dryunit/ppx_dryunit.ml
+++ src/ppx_dryunit/ppx_dryunit.ml
@@ -37,23 +37,19 @@ let rewriter _config _cookies =
   let expr self e =
     match e.pexp_desc with
 
-    (* dryunit *)
-    | Pexp_extension ({ txt = "dryunit"; _ }, PStr []) ->
-      { e with pexp_desc = Pexp_constant (Pconst_string ("Hello World", None)) }
-
-    (* dryunit_debug *)
+    (* debug just returns a string with detected tests *)
     | Pexp_extension ({ txt = "dryunit_debug"; _ }, PStr []) ->
       let output = Dryunit_core.debug ~filename:!Location.input_name in
       { e with pexp_desc = Pexp_constant (Pconst_string (output, None)) }
 
-    (* dryunit_alcotest *)
+    (* alcotest *)
     | Pexp_extension ({ txt = "alcotest"; _ }, PStr []) ->
       let filename = !Location.input_name in
       let name = title_from_no_padding @@
          Filename.(basename (chop_suffix filename ".ml")) in
       bootstrap_alcotest name (extract_from ~filename)
 
-    (* dryunit_ounit *)
+    (* ounit *)
     | Pexp_extension ({ txt = "ounit"; _ }, PStr []) ->
       let filename = !Location.input_name in
       let name = title_from_no_padding @@
