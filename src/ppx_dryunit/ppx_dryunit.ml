b65bf01be1451d2587f9837316e5d3aa55cfff3c
Gerson Moraes <gerson@digirati.com.br>
2017-08-26 15:44:48 -0300

    Added support for alcotest

--- src/ppx_dryunit/ppx_dryunit.ml
+++ src/ppx_dryunit/ppx_dryunit.ml
@@ -6,14 +6,6 @@ open Parsetree
 open Ast_helper
 open Ast_convenience
 
-
-let apply_unit ~e test_name =
-  let loc = e.pexp_loc in
-  Pexp_apply ({e with pexp_desc = Pexp_ident {txt = Lident test_name; loc}},
-  [(Nolabel, {e with pexp_desc = Pexp_construct ({txt = Lident "()"; loc}, None)})])
-
-
-
 let rewriter _config _cookies =
   let super = Ast_mapper.default_mapper in
   let expr self e =
@@ -29,10 +21,23 @@ let rewriter _config _cookies =
       { e with pexp_desc = Pexp_constant (Pconst_string (output, None)) }
 
     (* dryunit_debug_run *)
-    | Pexp_extension ({ txt = "dryunit_debug_run"; _ }, PStr []) ->
+    (* | Pexp_extension ({ txt = "dryunit_debug_run"; _ }, PStr []) ->
       let open Dryunit_core in
       let tests = extract_from ~filename:!Location.input_name in
-      { e with pexp_desc = apply_unit ~e (List.hd tests).test_name }
+      (* { e with pexp_desc = apply_unit (List.hd tests).test_name } *)
+      let test_name = (List.hd tests).test_name in
+      app (evar test_name) [unit ()] *)
+
+    (* dryunit_debug_run *)
+    | Pexp_extension ({ txt = "dryunit_alcotest"; _ }, PStr []) ->
+      let open Dryunit_core in
+      let test_set =
+        extract_from ~filename:!Location.input_name
+        |>  List.map (fun t -> tuple [ str t.test_title; Exp.variant "Quick" None; evar t.test_name ])
+        |> list
+       in
+       let test_set_list = list [ tuple [ str "test_set"; test_set ] ] in
+       app (evar "Alcotest.run") [ str "My first test"; test_set_list ]
 
     (* anything else *)
     | _ -> super.expr self e
--- tests/activation/main.ml
+++ tests/activation/main.ml
@@ -1,6 +1,14 @@
+module To_test = struct
+  let capit letter = Char.uppercase_ascii letter
+  let plus int_list = List.fold_left (fun a b -> a + b) 0 int_list
+end
 
-let test_execution () =
-  print_endline "action executed"
+let test_capit () =
+  Alcotest.(check char) "same chars"  'A' (To_test.capit 'a')
+
+let test_plus () =
+  Alcotest.(check int) "same ints" 7 (To_test.plus [1;1;2;3])
 
 let _ =
-  [%dryunit_debug_run]
+  let _ = [%dryunit_alcotest] in
+  ()
