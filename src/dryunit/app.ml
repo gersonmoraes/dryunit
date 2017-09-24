3d210b67b11bb870e3acbcdf4422dbb7e8848dc1
Gerson <gerson.xp@gmail.com>
2017-09-24 17:03:31 -0300

    Fix for config syntax

--- src/dryunit/app.ml
+++ src/dryunit/app.ml
@@ -24,7 +24,7 @@ let gen ~nocache ~framework ~cache_dir ~ignore ~filter ~targets =
     (Random.int 9999) + 1 in
   let msg = "This file is supposed to be generated before build with a random ID." in
   let id = format "%d%d%d" (get_int ()) (get_int ()) (get_int ()) in
-  ( format "(*\n  %s\n  ID = %s\n*)\nlet () =\n  [%%dryunit\n    {\n    ; cache_dir = %s\n    ; cache     = %s\n    ; framework = \"%s\"\n    ; ignore    = \"%s\"\n    ; filter    = \"%s\"\n    ; detection = \"%s\"\n    }\n  ]\n"
+  ( format "(*\n  %s\n  ID = %s\n*)\nlet () =\n  [%%dryunit\n    {\n    ; cache_dir = \"%s\"\n    ; cache     = %s\n    ; framework = \"%s\"\n    ; ignore    = \"%s\"\n    ; filter    = \"%s\"\n    ; detection = \"%s\"\n    }\n  ]\n"
     msg id cache_dir (string_of_bool @@ not nocache) framework ignore filter detection
   ) |>
   fun output ->
