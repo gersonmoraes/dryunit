ceef9cae640de6a2bf4a11cc3b238e425532a864
Gerson <gerson.xp@gmail.com>
2017-10-17 23:59:37 -0200

    Initial alcotest printing from exe

--- src/dryunit/core_serializer.ml
+++ src/dryunit/core_serializer.ml
@@ -1,3 +1,21 @@
 open Core_runtime
+open Printf
 
-(* let boot  *)
+open TestSuite
+open TestDescription
+
+let boot_alcotest ~oc suites =
+  fprintf oc "let () =\n";
+  fprintf oc "  Alcotest.run \"Name for the starter module\" [\n";
+  List.iter
+    ( fun suite ->
+      fprintf oc "    \"%s\", [\n" suite.suite_path;
+      List.iter
+        ( fun test ->
+          fprintf oc "      \"%s\", `Quick, %s.%s;\n" test.test_name suite.suite_name test.test_name;
+        )
+        suite.tests;
+      fprintf oc "];\n";
+    )
+    suites;
+fprintf oc "  ]\n";
