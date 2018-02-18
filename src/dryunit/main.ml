912d22dacc3448b976896728fdb2757b194a2ce5
Gerson <gerson.xp@gmail.com>
2018-02-18 20:13:49 -0300

    Improving help

--- src/dryunit/main.ml
+++ src/dryunit/main.ml
@@ -1,6 +1,6 @@
 open Cmdliner
 
-let version = "0.5.1"
+let version = "0.6.0"
 let sdocs = Manpage.s_common_options
 let exits = Term.default_exits
 
@@ -10,8 +10,6 @@ let help_secs = [
  `P "These options are common to all commands.";
  `S "MORE HELP";
  `P "Use `$(mname) $(i,COMMAND) --help' for help on a single command.";`Noblank;
- `P "Use `$(mname) help patterns' for help on patch matching."; `Noblank;
- `P "Use `$(mname) help environment' for help on environment variables.";
  `S Manpage.s_bugs; `P "Check bug reports at http://bugs.example.org.";
 ]
 
@@ -23,8 +21,8 @@ let gen_opts nocache framework cache_dir ignore only ignore_path targets =
 let gen_opts_t =
   let docs = "Generate source code for test executable with appropriate code to bootstrap a test framework" in
   let open Arg in
-  let nocache = value & flag & info ["nocache"] ~docs ~doc:"Do not use cache." in
-  let framework = value & opt (some string) None & info ["framework"] ~docs ~doc:"Select a test framework." in
+  let nocache = value & flag & info ["nocache"] ~docs ~doc:"Disable cache." in
+  let framework = value & opt (some string) None & info ["framework"] ~docs ~doc:"Define a test framework. Currently only 'alcotest' and 'ounit' are available. The default is 'alcotest'." in
   let cache_dir = value & opt (some string) None & info ["cache-dir"] ~docs ~doc:"Select a custom cache dir." in
   let only = value & opt (some string) None & info ["only"] ~docs ~doc:"Space separated list of words used to filter tests." in
   let ignore = value & opt (some string) None & info ["ignore"] ~docs ~doc:"Space separated list of words used to ignore tests." in
@@ -37,24 +35,27 @@ let init_opts framework =
   Action.( {framework } )
 let init_opts_t =
   let framework =
-    Arg.(value & pos 0 string "alcotest" & info [] ~docv:"FRAMEWORK" ~doc:"Select a framework") in
+    Arg.(value & pos 0 string "alcotest" & info [] ~docv:"FRAMEWORK" ~doc:"Define a test framework. The default is 'alcotest'.") in
   Term.(const init_opts $ framework)
 
 
 let init_cmd =
-  let doc = "shows a template configuration" in
+let doc = "The entrypoint of dryunit. To create a test suite, you can run: " ^
+  "`dryunit init > tests/jbuild` and you are all set. Detection should be " ^
+  "working and tests should be executed with `jbuilder runtest`. " ^
+  "To run a particular test generated with `dryunit init` passing parameters, " ^
+  "it's enough to run `jbuilder exec path/to/tests/main.exe -- --param1 --param2`" in
   Term.(ret (const Action.init_executable $ init_opts_t)),
   Term.info "init" ~doc ~sdocs ~exits
 
 
-let gen_extension_cmd =
-  let doc = "Generate dryunit initialization code" in
-  Term.(ret (const Action.gen_extension $ gen_opts_t)),
-  Term.info "extension" ~doc ~sdocs ~exits
-
-
 let gen_cmd =
-  let doc = "Generate dryunit initialization code" in
+  let doc =
+    "Generate bootstrap code for the main executable. " ^
+    "The entrypoint for dryunit is the command `dryunit init`, which generates
+    the jbuilder configuration responsible for setting up the bootstrap code gen.
+    Also, it's not advised to execute `dryunit gen` from a directory in source " ^
+    "control, because it generates compilation artifacts." in
   Term.(ret (const Action.gen_executable $ gen_opts_t)),
   Term.info "gen" ~doc ~sdocs ~exits
 
@@ -79,7 +80,6 @@ let default_cmd ~version =
 let cmds =
   [ init_cmd
   ; gen_cmd
-  ; gen_extension_cmd
   ; help_cmd
   ]
 
