d0c7798c3d3accdbe068ec5566d52f96e3d32608
Gerson <gerson@digirati.com.br>
2018-02-21 14:43:13 -0300

    Updating tests

--- test/modifiers/tests.ml
+++ test/modifiers/tests.ml
@@ -12,25 +12,33 @@ let len_should_be ?(msg="") ~path expected =
 
 
 let test_paths_without_mods () =
-  len_should_be 0 ~msg:"len zero" ~path:"tests.ml";
-  len_should_be 0 ~msg:"false positive" ~path:"async_cancelled_tests.ml";
-  ()
+  ( len_should_be 0 ~msg:"len zero" ~path:"tests.ml";
+    len_should_be 0 ~msg:"false positive" ~path:"async_cancelled_tests.ml";
+    len_should_be 0 ~msg:"false positive" ~path:"echain_cancelled_tests.ml";
+    len_should_be 0 ~msg:"false positive" ~path:"long_cancelled_tests.ml";
+    len_should_be 0 ~msg:"false positive" ~path:"result_cancelled_tests.ml";
+  )
+
 
 let test_requesting_only_async () =
   len_should_be 1 ~path:"async_tests.ml"
 
+
 let test_requesting_only_result () =
   len_should_be 1 ~path:"result_tests.ml"
+
 
 let test_requesting_only_echain () =
   len_should_be 1 ~path:"echain_tests.ml"
 
+
 let test_requesting_only_long () =
   len_should_be 1 ~path:"long_tests.ml"
 
+
 let tests_requiring_multiple_mods () =
-  len_should_be 2 ~path:"async_long_tests.ml";
-  len_should_be 2 ~path:"echain_result_tests.ml";
-  len_should_be 3 ~path:"echain_result_async_tests.ml";
-  len_should_be 4 ~path:"echain_long_result_async_tests.ml";
-  ()
+  ( len_should_be 2 ~path:"async_long_tests.ml";
+    len_should_be 2 ~path:"echain_result_tests.ml";
+    len_should_be 3 ~path:"echain_result_async_tests.ml";
+    len_should_be 4 ~path:"echain_long_result_async_tests.ml";
+  )
