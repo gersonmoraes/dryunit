7677d83e82f1afb6072f8886ddbf1cec73d38278
Gerson <gerson.xp@gmail.com>
2018-02-28 00:33:33 -0300

    Removing unused code

--- src/app.ml
+++ src/app.ml
@@ -4,33 +4,6 @@ open Model
 open Runtime
 
 
-let gen_extension ~nocache ~framework ~cache_dir ~ignore ~only ~targets ~ignore_path =
-  let _ = TestFramework.of_string framework in
-  let detection = "dir" in
-  let get_int () =
-    (Random.int 9999) + 1 in
-  let msg = "This file is supposed to be generated before build with a random ID." in
-  let id = sprintf "%d%d%d" (get_int ()) (get_int ()) (get_int ()) in
-  ( sprintf "(*\n  %s\n  ID = %s\n*)\nlet () =\n  [%%dryunit\n    { cache_dir   = \"%s\"\n    ; cache       = %s\n    ; framework   = \"%s\"\n    ; ignore      = \"%s\"\n    ; only        = \"%s\"\n    ; detection   = \"%s\"\n    ; ignore_path = \"%s\"\n    }\n  ]\n"
-      msg id cache_dir (string_of_bool @@ not nocache) framework ignore only detection ignore_path
-  ) |>
-  fun output ->
-  if List.length targets == 0 then
-    print_endline output
-  else
-    ( List.iter
-      ( fun target ->
-        let path = Sys.getcwd () ^ Filename.dir_sep ^ target in
-        let dir = Filename.dirname path in
-        Util.create_dir dir;
-        let oc = open_out path in
-        Printf.fprintf oc "%s" output;
-        close_out oc
-      )
-      targets
-    )
-
-
 let throw s =
   Printf.eprintf "%s\n" s;
   exit 1
